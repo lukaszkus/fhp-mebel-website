af1dfac213ff9c303be4915274630bd3fb9a5523
lukaszkus <kusiu@vp.pl>
2022-04-05 14:40:04 +0200

    Navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,10 +5,17 @@ import { images } from "../../constants";
 
 function Navbar() {
   return (
-    <nav>
-      <div>
+    <nav className="navbar">
+      <div className="navbar__logo">
         <img src={images.logo} alt="logo" />
       </div>
+      <ul className="navbar__links">
+        {["Co robimy", "Realizacje", "O nas", "Kontakt"].map((item) => (
+          <li key={`link-${item}`}>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
