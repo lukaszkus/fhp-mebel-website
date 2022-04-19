d311ea108d88554461901b6a05a41b109aa43a86
lukaszkus <kusiu@vp.pl>
2022-04-19 11:03:47 +0200

    Contact cards added

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,8 @@
 //MAIN ASSETS
 import logo from "../assets/fhpmebel_logo.svg";
+import marker from "../assets/marker.svg";
+import email from "../assets/email.svg";
+import phone from "../assets/phone.svg";
 
 //Slider
 import kitchen from "../assets/slide-kitchen.jpg";
@@ -23,15 +26,21 @@ import cat_szafy from "../assets/portfolio/cat_szafy.jpg";
 
 const images = {
   logo,
+  marker,
+  email,
+  phone,
+
   kitchen,
   wardrobe,
   bathroom,
   renovations,
+
   kuchnia,
   lazienka,
   remonty,
   salon,
   szafa,
+
   cat_kuchnie,
   cat_lazienki,
   cat_remonty,
--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+// import {
+//   HiOutlineLocationMarker,
+//   HiOutlineMail,
+//   HiOutlinePhone,
+// } from "react-icons/hi";
+
 // import { motion } from "framer-motion";
 
 import "./Contact.scss";
-// import { images } from "../../constants";
+import { images } from "../../constants";
 import { SectionHeading } from "../../components";
 
 function Contact() {
@@ -12,11 +18,29 @@ function Contact() {
         <SectionHeading heading="Kontakt" />
         <div className="contact__container">
           <div className="contact__cards">
-            <div className="contact__card contact__card--adress">
-              <h2 className="contact__card-title">Title</h2>
+            <div className="contact__card">
+              <img
+                src={images.marker}
+                className="contact__icon"
+                alt="address"
+              />
+              <h2 className="contact__card-title">Adres:</h2>
+              <p className="contact__text">
+                ul. Pod Fortem 19
+                <br />
+                Kraków
+              </p>
             </div>
-            <div className="contact__card contact__card--email"></div>
-            <div className="contact__card contact__card--phone"></div>
+            <div className="contact__card">
+              <img src={images.email} className="contact__icon" alt="email" />
+              <h2 className="contact__card-title">Email:</h2>
+              <p className="contact__text">biuro@fhpmebel.pl</p>
+            </div>
+            <div className="contact__card">
+              <img src={images.phone} className="contact__icon" alt="phone" />
+              <h2 className="contact__card-title">Telefon:</h2>
+              <p className="contact__text">602 692 161</p>
+            </div>
           </div>
           <div className="contact__map"></div>
         </div>
